4891f269428a31885cde9d97a6f829526030bcda
AlekseySemma <alekseysemma@gmail.com>
2026-03-17 10:47:00 +0300

    Исправил: merge revision был слишком длинный для alembic_version.version_num. Теперь у merge-миграции короткий ID: 0021_merge_email_template.

--- alembic/versions/0021_merge_template_department_and_email_verification.py
+++ alembic/versions/0021_merge_template_department_and_email_verification.py
@@ -1,13 +1,13 @@
 """Merge template department and email verification heads.
 
-Revision ID: 0021_merge_template_department_and_email_verification
+Revision ID: 0021_merge_email_template
 Revises: 0020_template_department, 0020_user_email_verification
 Create Date: 2026-03-17
 """
 from __future__ import annotations
 
 
-revision = "0021_merge_template_department_and_email_verification"
+revision = "0021_merge_email_template"
 down_revision = ("0020_template_department", "0020_user_email_verification")
 branch_labels = None
 depends_on = None
